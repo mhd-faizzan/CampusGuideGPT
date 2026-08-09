08e68ac76193b15884ad622b3c519639d7bcf6ee
mhd-faizzan <muhammadfaizzan@outlook.com>
2026-08-09 22:04:43 +0200

    fix firebase.js location, add firebase dep

---
+++ frontend/src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCk2HherDx1DmOwG4lCOoZeApOstkwkQyc",
+  authDomain: "campusguidegpt-93357.firebaseapp.com",
+  projectId: "campusguidegpt-93357",
+  storageBucket: "campusguidegpt-93357.firebasestorage.app",
+  messagingSenderId: "951554938433",
+  appId: "1:951554938433:web:e9524bfd7c522c190e18c0",
+  measurementId: "G-6GQG8T8WLC"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
